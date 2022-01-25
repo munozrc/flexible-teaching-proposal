443accdd106050d7482de7baec1d28a99f374238
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-25 00:26:33 -0500

    feat(Root): Add share button feat

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 import '../css/style.css'
 import '../css/theme.css'
+
+const btnShare = document.querySelector('#btnShare')
+
+btnShare.addEventListener('click', () => {
+  if (!navigator.share) throw Error('Navigator Share API no supporter')
+
+  navigator.share({
+    title: 'Propuesta Enseñanza Flexible',
+    url: 'https://propuesta-ensenanza-flexible.netlify.app/',
+    text: ''
+  }).then(() => console.log('shared successfully')).catch(err => console.log({ err }))
+})
