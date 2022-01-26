25a9c97303ae65cb81ef98a926c472f5f6a0ef7f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-25 20:32:56 -0500

    feat(Root): Add styles and add section itinerary

--- js/areas.js
+++ js/areas.js
@@ -30,6 +30,16 @@ const articles = [
     title: 'Artes',
     image: 'https://images.unsplash.com/photo-1596464716127-f2a82984de30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
     content: 'Hacer de la vida una obra de arte mediante la ilustración desde la vegetación, los animales, las historias, porcelana fria, origami (aplica matemáticas).'
+  },
+  {
+    title: 'Pensamiento Matemático',
+    image: 'https://images.unsplash.com/photo-1529078155058-5d716f45d604?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80',
+    content: 'Hacer de la vida una obra de arte mediante la ilustración desde la vegetación, los animales, las historias, porcelana fria, origami (aplica matemáticas).'
+  },
+  {
+    title: 'Pensamiento Escrito',
+    image: 'https://images.unsplash.com/photo-1550592704-6c76defa9985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    content: 'Hacer de la vida una obra de arte mediante la ilustración desde la vegetación, los animales, las historias, porcelana fria, origami (aplica matemáticas).'
   }
 ]
 
