7adba1efbd7aba4dc6e2bb1029b6ab323bff784f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-01 14:50:57 -0500

    feat(Root): new change any texts

--- js/areas.js
+++ js/areas.js
@@ -19,7 +19,7 @@ const articles = [
   {
     title: 'Ajedréz',
     image: 'https://images.unsplash.com/photo-1529699211952-734e80c4d42b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
-    content: 'Quien este interesado, adquiere su herramienta pedagogía.'
+    content: 'Quien este interesado, adquiere su herramienta pedagógica.'
   },
   {
     title: 'Inglés',
